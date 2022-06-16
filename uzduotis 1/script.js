cc5929d0881e1bb87c8c799b284782a8818093c8
edgo1 <vytasrytas@gmail.com>
2022-06-16 14:43:55 +0300

    darbai klaseje

---
+++ uzduotis 1/script.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    const skarry = [5, 12, 3, 6, 8, 14, 15, 5, 6, 10];
+    let ieskomasSkaicius = 14;
+    let ieskomasSkaicius2 = 10; 
+    console.log(arIeskomasSkaiciusYraMasyve(skarry,ieskomasSkaicius,ieskomasSkaicius2))
+}
+
+function arIeskomasSkaiciusYraMasyve(arry,x,y) {
+    let sk = false;
+    let sk2 = false; 
+    for (let i = 0; i < arry.length; i++) {
+        if(arry[i] === x){
+            sk = true
+        }
+        if (arry[i] === y) {
+            sk2 = true
+            }
+        if (sk && sk2 === true) {
+            return true
+            }
+        
+    }
+    return false
+}
